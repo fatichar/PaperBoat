7af8082b44a0fc3ddc8ccdcf569b0e27160ad936
Pravin Chaudhary <pravin.chaudhary@gmail.com>
2024-05-11 14:12:11 +0530

    Added app, displayed user selected pdg (1st page)

--- App/App/Views/MainView.axaml.cs
+++ App/App/Views/MainView.axaml.cs
@@ -1,11 +1,41 @@
+using System.Collections.Generic;
+using App.ViewModels;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.VisualTree;
 
 namespace App.Views;
 
 public partial class MainView : UserControl
 {
+    private MainViewModel? Context => (MainViewModel?)DataContext;
+
     public MainView()
     {
         InitializeComponent();
+    }
+
+    private void LoadDocumentButton_OnClick(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void BrowseButton_Click(object? sender, RoutedEventArgs e)
+    {
+        var dialog = new OpenFileDialog
+        {
+            Title = "Select a document",
+            Filters = new List<FileDialogFilter>
+            {
+                new FileDialogFilter { Name = "PDF Files", Extensions = { "pdf" } },
+                new FileDialogFilter { Name = "Image Files", Extensions = { "png" } }
+            }
+        };
+        var result = dialog.ShowAsync(GetMainWindow());
+    }
+
+    private Window? GetMainWindow()
+    {
+        return this.GetVisualRoot() as Window;
     }
 }
