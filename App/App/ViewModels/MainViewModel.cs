549b9ad9708b775680e6ecca59405efa4633fe22
Pravin Chaudhary <pravin.chaudhary@taranawireless.com>
2024-05-12 09:13:38 +0530

    Added basic UI

--- App/App/ViewModels/MainViewModel.cs
+++ App/App/ViewModels/MainViewModel.cs
@@ -4,5 +4,6 @@ public class MainViewModel : ViewModelBase
 {
 #pragma warning disable CA1822 // Mark members as static
     public string Greeting => "Welcome to PaperBoat!";
+    public string DocumentImagePath { get; } = "Assets/sample_doc.png";
 #pragma warning restore CA1822 // Mark members as static
 }
--- App/App/Views/MainView.axaml.cs
+++ App/App/Views/MainView.axaml.cs
@@ -1,10 +1,15 @@
+using System.Collections.Generic;
+using App.ViewModels;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.VisualTree;
 
 namespace App.Views;
 
 public partial class MainView : UserControl
 {
+    private MainViewModel? Context => (MainViewModel?)DataContext;
+
     public MainView()
     {
         InitializeComponent();
@@ -17,6 +22,20 @@ public partial class MainView : UserControl
 
     private void BrowseButton_Click(object? sender, RoutedEventArgs e)
     {
-        throw new System.NotImplementedException();
+        var dialog = new OpenFileDialog
+        {
+            Title = "Select a document",
+            Filters = new List<FileDialogFilter>
+            {
+                new FileDialogFilter { Name = "PDF Files", Extensions = { "pdf" } },
+                new FileDialogFilter { Name = "Image Files", Extensions = { "png" } }
+            }
+        };
+        var result = dialog.ShowAsync(GetMainWindow());
+    }
+
+    private Window? GetMainWindow()
+    {
+        return this.GetVisualRoot() as Window;
     }
 }
